20833988814931cfdbb233b984b170fe191d2884
marcosprofile <marcos_laurindo1997@hotmail.com>
2023-07-17 01:00:55 -0300

    finished project to-do-list

--- src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-itens',
   templateUrl: './todo-input-add-itens.component.html',
   styleUrls: ['./todo-input-add-itens.component.scss']
 })
-export class TodoInputAddItensComponent {
+export class TodoInputAddItensComponent implements OnInit {
 
+  @Output() public emitItemTaskList = new EventEmitter();
+
+  public addItemTaskList: string = "";
+  constructor() {  }
+
+  ngOnInit(): void {
+  }
+
+  public submitItemTaskList() {
+    this.addItemTaskList = this.addItemTaskList.trim();
+
+    if (this.addItemTaskList) {
+      this.emitItemTaskList.emit(this.addItemTaskList);
+      this.addItemTaskList = "";
+    }
+  }
 }
--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck,  } from '@angular/core';
+
+// Interface
+import { TaskList } from './../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
 
+export class TodoListComponent implements DoCheck {
+
+  public taskList: Array<TaskList> = JSON.parse(localStorage.getItem('list') || '[]');
+
+  constructor() {  }
+
+  ngDoCheck() {
+    this.setLocalStorage();
+  }
+
+  public setEmitTaskList(event: string) {
+    this.taskList.push({ task: event, checked: false });
+  }
+
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList() {
+    const confirm = window.confirm('Você deseja realmente Deletar tudo?')
+
+    if (confirm) {
+      this.taskList = [];
+    }
+  }
+
+  public validationInput(event: string, index: number) {
+    if (!event.length) {
+      const confirm = window.confirm('Task está vazia, deseja Deletar?');
+
+      if (confirm) {
+        this.deleteItemTaskList(index);
+      }
+    }
+  }
+
+  public setLocalStorage() {
+    if (this.taskList) {
+      this.taskList.sort( (first, last) => Number(first.checked) - Number(last.checked))
+      localStorage.setItem("list", JSON.stringify(this.taskList));
+    }
+  }
 }
